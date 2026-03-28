418f0f00056f0d2a258fa8d6d5936b0be9fabc81
julio <julio.lisboa@a.ucb.br>
2026-03-28 14:47:14 -0300

    inicio

---
+++ consulta.h
@@ -0,0 +1,20 @@
+#ifndef CONSULTA_H
+#define CONSULTA_H
+
+
+// ./consulta-tre -b -m dados.csv
+// ./consulta-tre -c -p pastaComDados
+// ./consulta-tre -c dados1.csv dados2.csv ...
+// ./consulta-tre -r dados.csv
+// ./consulta-tre -h
+// ./consulta-tre -v 
+static const char comando_buscar = "-b";
+static const char comando_concatenar = "-c";
+static const char comando_resumir = "-r";
+static const char comando_ajuda = "-h";
+static const char opt_pasta = "-p";
+
+
+
+
+#endif
