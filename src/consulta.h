0808f9c32b3e134648f77eeac89ed74fff8ed313
Júlio César <132522218+juliodf24@users.noreply.github.com>
2026-04-07 14:06:38 +0000

    teste

---
+++ src/consulta.h
@@ -0,0 +1,54 @@
+#ifndef CONSULTA_H
+#define CONSULTA_H
+#define ERRO -1
+typedef struct {
+	char *cmd; //string do comando Ex: "-h"
+    // armazena um ponteiro para uma função Ex: cmd_help
+	int (*fn)(int, char **); // int para receber o argc e char** para receber o endereco de argv
+} comando_struct;
+
+
+// deve ser tratado tudo como ponteiro pois na logica utilizada na leitura do csv o calculo de quantidade de campos é feita utilizando sizeof(UnidadeJurisdiciona_Struct)/8 
+typedef struct{
+	char* sigla_tribunal;
+	char* procedimento;
+	char* ramo_justica;
+	char* sigla_grau;
+	char* uf_oj;
+	char* municipio_oj;
+	char* id_ultimo_oj;
+	char* nome;
+	char* mesano_cnm1;
+	char* mesano_sent;
+	char* casos_novos_2026;
+	char* julgados_2026;
+	char* prim_sent2026;
+	char* suspensos_2026;
+	char* dessobrestados_2026;
+	char* cumprimento_meta1;
+	char* distm2_a;
+	char* julgm2_a;
+	char* suspm2_a;
+	char* cumprimento_meta2a;
+	char* distm2_ant;
+	char* julgm2_ant;
+	char* suspm2_ant;
+	char* desom2_ant;
+	char* cumprimento_meta2ant;
+	char* distm4_a;
+	char* julgm4_a;
+	char* suspm4_a;
+	char* cumprimento_meta4a;
+	char* distm4_b;
+	char* julgm4_b;
+	char* suspm4_b;
+	char* cumprimento_meta4b;
+}UnidadeJurisdiciona_Struct;
+
+
+int cmd_help(int argc, char **argv);
+int cmd_buscar(int argc, char **argv);
+int cmd_versao(int argc, char **argv);
+int cmd_concatenar(int argc, char **argv);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "consulta.h"
+
+// Lista de comandos
+// O static indica que seu valor é persistente e que ele só é acessível dentro deste arquivo
+static comando_struct comandos[] = {
+    {"-h", cmd_help},
+    {"-c", cmd_concatenar},
+    {"-b", cmd_buscar},
+    {"-v", cmd_versao}
+}; // cadastra os comandos na lista de comandos
+
+// recebe uma string e retorna o comando no formato comando_struct caso exista, se não retorna null
+static comando_struct *encontrarComando(char *texto){
+    for (int i = 0; i < (sizeof(comandos)/sizeof(comandos[0])); i++){
+        comando_struct *comando = &comandos[i];
+        if(strcmp(texto, comando->cmd) == 0){
+            return comando;
+        }
+    } return NULL;
+} 
+
+int main(int argc, char *argv[]){
+
+    char *cmd; 
+    
+    if (argc < 2) {
+        cmd = "-h";
+    } else {
+        cmd = argv[1];
+    }
+
+    comando_struct *comando = encontrarComando(cmd);
+    argc--;
+    argv++;
+    if(comando){
+        int resultado = comando->fn(argc , argv++);
+    } else{
+        printf("Comando não encontrado!\n");
+        encontrarComando("-h")->fn(argc ,argv);
+        exit(0);
+    }
+
+    return 0;
+}
